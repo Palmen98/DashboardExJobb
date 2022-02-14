9cc949da423c8f029fed42067c5674ed88122c3e
Ryan Bui <rb222rx@student.lnu.se>
2022-02-14 14:59:26 +0100

    chart cart

--- src/client/postcss.config.js
+++ src/client/postcss.config.js
@@ -1,6 +1,8 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+/* eslint-disable import/no-anonymous-default-export */
+import tailwind from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+import tailwindConfig from './tailwind.config.js';
+
+export default {
+  plugins: [tailwind(tailwindConfig), autoprefixer],
+};
--- src/client/src/components/HalfDonoutChart.tsx
+++ src/client/src/components/HalfDonoutChart.tsx
@@ -7,14 +7,8 @@ export default function HalfDonoutChart() {
     datasets: [
       {
         data: [40, 60],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-        ],
-        borderColor: [
-          'rgba(255,99,132,1)',
-          'rgba(54, 162, 235, 1)',
-        ],
+        backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)'],
+        borderColor: ['rgba(255,99,132,1)', 'rgba(54, 162, 235, 1)'],
       },
     ],
   };
@@ -22,11 +16,12 @@ export default function HalfDonoutChart() {
   const options = {
     rotation: -90,
     circumference: 180,
+    maintainAspectRatio: false,
   };
 
   return (
     <div>
-      <Doughnut data={data} options={options} width={200} height={200} />
+      <Doughnut data={data} options={options} width={389} height={260} />
     </div>
   );
 }
