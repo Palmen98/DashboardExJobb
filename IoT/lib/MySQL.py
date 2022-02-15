21d31514a6c0e9dee33748029941e2359b85483b
Palmen98 <simon.palm98@hotmail.se>
2022-02-15 10:19:01 +0100

    Adding how to create database and changing some variables.

--- IoT/lib/MySQL.py
+++ IoT/lib/MySQL.py
@@ -10,23 +10,27 @@ db=mysql.connector.connect(host = 'localhost',
 
 mycursor = db.cursor()
 
+
+# Create database
+# mycursor.execute('CREATE DATABASE ' + keys.MYSQL_DATABASE_NAME)
+
 # Create table
-# mycursor.execute("CREATE TABLE Sensors (name VARCHAR(50), type VARCHAR(50), data int, sensorID INT PRIMARY KEY AUTO_INCREMENT)")
+# mycursor.execute("CREATE TABLE Sensor (name VARCHAR(50), type VARCHAR(50), data int, sensorID INT PRIMARY KEY AUTO_INCREMENT)")
 
 # Create 
-# mycursor.execute('INSERT INTO Sensors (name,type) VALUES (%s,%s)', ('UltraSonicSensor', 'Distance measure'))
+# mycursor.execute('INSERT INTO Sensor (name,type) VALUES (%s,%s)', ('UltraSonicSensor', 'Distance measure'))
 # db.commit()
 
 # Update
-# mycursor.execute('UPDATE Sensors SET data = 100 WHERE sensorID = 1')
+# mycursor.execute('UPDATE Sensor SET data = 100 WHERE sensorID = 1')
 # db.commit()
 
 # Select a table
-mycursor.execute('SELECT * FROM Sensors')
+# mycursor.execute('SELECT * FROM Sensor')
 
 # Remove a table
-# mycursor.execute('DROP TABLE Person')
+# mycursor.execute('DROP TABLE Sensor')
 
 # Loops through all data in table
-for x in mycursor:
-    print(x)
+# for x in mycursor:
+#     print(x)
