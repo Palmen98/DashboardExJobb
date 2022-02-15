2975ed9f9425eef767bc6876c4b6b35285fe3780
Palmen98 <simon.palm98@hotmail.se>
2022-02-15 11:03:27 +0100

    Creating functions for MySQL functionality.

--- IoT/lib/MySQL.py
+++ IoT/lib/MySQL.py
@@ -1,36 +1,44 @@
 import mysql.connector
 import Keys as keys
 
-db=mysql.connector.connect(host = 'localhost',
+def ConnectDB():
+    db = mysql.connector.connect(host = 'localhost',
     user = keys.MYSQL_USER,
     passwd = keys.MYSQL_PASSWORD,
     database = keys.MYSQL_DATABASE_NAME
-)
+    )
+    return db
 
-
+db = ConnectDB()    
 mycursor = db.cursor()
 
-
 # Create database
-# mycursor.execute('CREATE DATABASE ' + keys.MYSQL_DATABASE_NAME)
+def CreateDB(): 
+    mycursor.execute('CREATE DATABASE ' + keys.MYSQL_DATABASE_NAME)
 
 # Create table
-# mycursor.execute("CREATE TABLE Sensor (name VARCHAR(50), type VARCHAR(50), data int, sensorID INT PRIMARY KEY AUTO_INCREMENT)")
+def CreateTable():
+    mycursor.execute("CREATE TABLE Sensor (name VARCHAR(50), type VARCHAR(50), data int, sensorID INT PRIMARY KEY AUTO_INCREMENT)")
 
-# Create 
-# mycursor.execute('INSERT INTO Sensor (name,type) VALUES (%s,%s)', ('UltraSonicSensor', 'Distance measure'))
-# db.commit()
+# Insert data into table
+def InsertDataToTable():
+    mycursor.execute('INSERT INTO Sensor (name,type) VALUES (%s,%s)', ('UltraSonicSensor', 'Distance measure'))
+    db.commit()
 
-# Update
-# mycursor.execute('UPDATE Sensor SET data = 100 WHERE sensorID = 1')
-# db.commit()
+# Update data
+def UpdateData():
+    mycursor.execute('UPDATE Sensor SET data = 100 WHERE sensorID = 1')
+    db.commit()
 
 # Select a table
-# mycursor.execute('SELECT * FROM Sensor')
+def SelectDataFromTable():
+    mycursor.execute('SELECT * FROM Sensor')
 
 # Remove a table
-# mycursor.execute('DROP TABLE Sensor')
+def DeleteTable():
+    mycursor.execute('DROP TABLE Sensor')
 
-# Loops through all data in table
-# for x in mycursor:
-#     print(x)
+# Loops through all data in the selected table
+SelectDataFromTable()
+for data in mycursor:
+    print(data)
