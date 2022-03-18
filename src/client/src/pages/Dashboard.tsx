a419e41d051288c3c2166e1ccd07544bb990e05e
Palmen98 <simon.palm98@hotmail.se>
2022-03-18 18:21:52 +0100

    Adding components to page.

--- src/client/src/pages/Dashboard.tsx
+++ src/client/src/pages/Dashboard.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import DashboardCard from '../components/DashboardCard';
+import EnvironmentalHouseActivity from '../components/EnvironmentalHouseActivity';
+import Navbar from '../components/Navbar';
 
+import GoogleMapsLocation from '../components/GoogleMapsLocation';
 export default function Dashboard() {
   return (
-    <div className='flex h-screen overflow-hidden'>
+    <div className='flex h-screen overflow-hidden bg-gray-300'>
       <div className='relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden'>
         <main>
+          <nav>
+            <Navbar />
+          </nav>
+              <GoogleMapsLocation />
           <div className='px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto'>
             <div className='sm:flex sm:justify-between sm:items-center mb-8'></div>
 
             <div className='grid grid-cols-12 gap-6'>
                 <DashboardCard />
+                <EnvironmentalHouseActivity />
             </div>
           </div>
         </main>
