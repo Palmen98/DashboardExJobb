062991660beef8e567470207ad3210e83e5aefaa
Palmen98 <simon.palm98@hotmail.se>
2022-03-23 12:28:21 +0100

    Creating a function to reuse with different devices to connect to lora.

--- lib/lora.py
+++ lib/lora.py
@@ -5,55 +5,41 @@ import ubinascii
 import lib.keys as keys
 import lib.light_manager as light_manager
 
-# Initialise LoRa in LORAWAN mode.
-# Please pick the region that matches where you are using the device:
-# Asia = LoRa.AS923
-# Australia = LoRa.AU915
-# Europe = LoRa.EU868
-# United States = LoRa.US915
-lora = LoRa(mode=LoRa.LORAWAN, region=LoRa.EU868)
+def startLoraConnection(device_app_eui, device_app_key, device_dev_eui):
+    lora = LoRa(mode=LoRa.LORAWAN, region=LoRa.EU868)
+    # create an OTAA authentication parameters, change them to the provided credentials
+    app_eui = ubinascii.unhexlify(keys.app_eui_magnet_sensor)
+    app_key = ubinascii.unhexlify(keys.app_key_magnet_sensor)
+    #uncomment to use LoRaWAN application provided dev_eui
+    # dev_eui = ubinascii.unhexlify(device_dev_eui)
+    # join a network using OTAA (Over the Air Activation)
+    #uncomment below to use LoRaWAN application provided dev_eui
+    lora.join(activation=LoRa.OTAA, auth=(app_eui, app_key), timeout=0)
+    #lora.join(activation=LoRa.OTAA, auth=(dev_eui, app_eui, app_key), timeout=0)
 
-# create an OTAA authentication parameters, change them to the provided credentials
-app_eui = ubinascii.unhexlify(keys.app_eui)
-app_key = ubinascii.unhexlify(keys.app_key)
-#uncomment to use LoRaWAN application provided dev_eui
-dev_eui = ubinascii.unhexlify(keys.dev_eui)
+    # wait until the module has joined the network
+    while not lora.has_joined():
+        light_manager.look_foor_connection() # show with light that it still looking
+        print('Not yet joined...')
+        time.sleep(3)
 
-# Uncomment for US915 / AU915 & Pygate
-# for i in range(0,8):
-#     lora.remove_channel(i)
-# for i in range(16,65):
-#     lora.remove_channel(i)
-# for i in range(66,72):
-#     lora.remove_channel(i)
+    print('Joined Network')
+    light_manager.connected() # show with lights that it find network
+    # create a LoRa socket
+    s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
 
-# join a network using OTAA (Over the Air Activation)
-#uncomment below to use LoRaWAN application provided dev_eui
-lora.join(activation=LoRa.OTAA, auth=(app_eui, app_key), timeout=0)
-#lora.join(activation=LoRa.OTAA, auth=(dev_eui, app_eui, app_key), timeout=0)
+    # set the LoRaWAN data rate
+    s.setsockopt(socket.SOL_LORA, socket.SO_DR, 5)
 
-# wait until the module has joined the network
-while not lora.has_joined():
-    light_manager.look_foor_connection() # show with light that it still looking
-    print('Not yet joined...')
-    time.sleep(3)
+    # make the socket blocking
+    # (waits for the data to be sent and for the 2 receive windows to expire)
+    s.setblocking(True)
 
-print('Joined Network')
-light_manager.connected() # show with lights that it find network
-# create a LoRa socket
-s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+    # make the socket non-blocking
+    # (because if there's no data received it will block forever...)
+    s.setblocking(False)
 
-# set the LoRaWAN data rate
-s.setsockopt(socket.SOL_LORA, socket.SO_DR, 5)
+    # get any data received (if any...)
+    data = s.recv(64)
+    print(data)
 
-# make the socket blocking
-# (waits for the data to be sent and for the 2 receive windows to expire)
-s.setblocking(True)
-
-# make the socket non-blocking
-# (because if there's no data received it will block forever...)
-s.setblocking(False)
-
-# get any data received (if any...)
-data = s.recv(64)
-print(data)
