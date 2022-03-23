82c1b34f631b7cc0e746f49ba046f358b34902fe
Palmen98 <simon.palm98@hotmail.se>
2022-03-23 12:27:59 +0100

    Sending data to helium.

--- lib/door_magnet.py
+++ lib/door_magnet.py
@@ -1,13 +1,27 @@
 import time
-import wifi
+# import wifi
 from machine import Pin
+import lora 
+import keys
+import light_manager
+
+lora.startLoraConnection(keys.app_eui_magnet_sensor, keys.app_key_magnet_sensor, keys.dev_eui_magnet_sensor)
 
 
 # wifi.connectToWifi()
 magnet = Pin('P18', mode=Pin.IN)
 
+def sendData(port, pin):
+    lora.s.bind(port)
+    lora.s.send(bytes(pin))
+
 def start_magnet():
+    while True:
         magnetIsActive = magnet()
         if magnetIsActive == 0:
             print('Door open')
-            return magnetIsActive
+            # sendData(18, magnetIsActive)
+            # light_manager.sendData()
+            # print('Sending data')
+            time.sleep(10)
+            # return magnetIsActive
