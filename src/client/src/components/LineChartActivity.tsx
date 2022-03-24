a3832eb5fe6a68154925f01bd4512c56a8742690
Palmen98 <simon.palm98@hotmail.se>
2022-03-24 14:51:57 +0100

    Fetching api data.

--- src/client/src/components/LineChartActivity.tsx
+++ src/client/src/components/LineChartActivity.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Line } from 'react-chartjs-2';
 import 'chart.js/auto';
 
 export default function LineChartActivity() {
+  const [ActivityData, setActivityData] = useState()
+
+  useEffect(() => {
+    const activityData = fetch('https://environmental-house-dashboard.firebaseapp.com/api/magnet')
+    .then(response => response.json())
+    .then(data => setActivityData(data));
+    console.log(activityData)
+    console.log(ActivityData)
+    return () => {
+      
+    }
+  }, [])
+  
+    
+
     const data:any = {
         labels: ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00', 
         '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00'],
