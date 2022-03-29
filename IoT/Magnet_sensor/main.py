266abf74b34212d355f3c91312023af5dc26ee56
Palmen98 <simon.palm98@hotmail.se>
2022-03-29 18:23:12 +0200

    importing wlan helper.

--- IoT/Magnet_sensor/main.py
+++ IoT/Magnet_sensor/main.py
@@ -1,5 +1,10 @@
 import time
+from lib.wlanhelper import wlanhelper
 import lib.Magnet_sensor as magnet
+
+wlan = wlanhelper()
+
+print(wlan.ifconfig()[0])
 
 print('Starting program...')
 magnetIsActive = magnet.start_magnet()
